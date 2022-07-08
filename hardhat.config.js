d60e671f9505fa75f1563c994f177d2a5e721805
Vlad Glupak <vglupak@Vlads-MBP.hsd1.ca.comcast.net>
2022-07-07 22:44:48 -0700

    initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,38 @@
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
+require("@nomiclabs/hardhat-etherscan");
+require("solidity-coverage");
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL || "provide address";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "provide key";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "provide key";
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    rinkeby: {
+      url: RINKEBY_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 4,
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      chainId: 31337,
+    },
+    ganache: {
+      url: "http://127.0.0.1:8545",
+      chainId: 1337,
+    },
+  },
+  solidity: "0.8.7",
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+};
